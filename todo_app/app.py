341f6ded43a02d4bb53f48bfafa88f88899ec19e
shaylipatel <80635019+shaylipatel@users.noreply.github.com>
2023-07-30 21:13:28 +0100

    Create route to add a new item to list. Updated the html file and added a button

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item
@@ -6,8 +6,14 @@ from todo_app.data.session_items import get_items, add_item
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
+@app.route('/items')
 @app.route('/')
 def index():
     items = get_items()
     return render_template('index.html', items=items)
+
+@app.route('/items/new', methods=['POST'])
+def add_item():
+    title = request.form.get('field_name')
+    add_item(title)
+    return redirect(url_for('index'))
