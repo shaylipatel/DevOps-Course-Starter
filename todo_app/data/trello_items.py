43e31579aaff95f5dcf28109af978e0295f5fbf2
shaylipatel <80635019+shaylipatel@users.noreply.github.com>
2023-09-12 18:49:35 +0100

    created trello_items.py and the get_items, get_lists and _select_fields functions in app.py, added the board, trello api key and toek and list name variables and imported function from trello_items not session_items.

---
+++ todo_app/data/trello_items.py
@@ -0,0 +1,68 @@
+from flask import requests
+
+_DEFAULT_ITEMS=[]
+# board_id = os.getenv(TRELLO_BOARD_ID)
+# trello_token = os.getenv(TRELLO_TOKEN)
+# trello_key = os.getenv(TRELLO_KEY)
+
+
+def get_items(board_id, trello_token, trello_key, list_names):
+    """
+    Fetches all saved items from the session.
+
+    Returns:
+        list: The list of saved items.
+    """
+
+    trello_get_cards_api = requests.get(f"https://api.trello.com/1/boards/{board_id}/cards/open?key={trello_key}&token={trello_token}").json
+    _DEFAULT_ITEMS = _select_fields(trello_get_cards_api)
+    return trello_get_cards_api
+
+def _select_fields(data, list_names):
+    for record in data:
+        id = record['id']
+        vals = [list['id'] for list in _DEFAULT_ITEMS]
+        if id not in vals:
+            title = record['name']
+            list_id = record["idList"]
+            status = list_names[list_id]
+            new_item = {"id": id, "status": status ,"title": title }
+            _DEFAULT_ITEMS.append(new_item)
+    return _DEFAULT_ITEMS
+
+
+def get_lists(board_id,trello_token, trello_key):
+    trello_lists_api = requests.get(f"https://api.trello.com/1/boards/{board_id}/lists?key={trello_key}&token={trello_token}").json
+    list_names = {}
+    for list in trello_lists_api:
+        list_names[list['id']] = list['name']
+    return list_names
+        
+def get_item(id):
+    """
+    Adds a new item with the specified title to the session.
+
+    Args:
+        title: The title of the item.
+
+    Returns:
+        item: The saved item.
+    """
+
+def add_item(title):
+    """
+    Adds a new item with the specified title to the session.
+
+    Args:
+        title: The title of the item.
+
+    Returns:
+        item: The saved item.
+    """
+def save_item(item):
+    """
+    Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
+
+    Args:
+        item: The item to save.
+    """
