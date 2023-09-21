a917e8b452941c40baecc79e20f907883524cdbf
shaylipatel <80635019+shaylipatel@users.noreply.github.com>
2023-09-21 14:15:46 +0100

    Created functions in trello_items.py to get trello api data and list it in the table

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import os
 from todo_app.flask_config import Config
 # from todo_app.data.session_items import get_items, add_item
-from todo_app.data.trello_items import get_item, get_items, get_lists
+from todo_app.data.trello_items import get_items, get_lists
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-board_id = os.getenv(TRELLO_BOARD_ID)
-trello_token = os.getenv(TRELLO_TOKEN)
-trello_key = os.getenv(TRELLO_KEY)
+board_id = os.getenv('TRELLO_BOARD_ID')
+trello_token = os.getenv('TRELLO_TOKEN')
+trello_key = os.getenv('TRELLO_KEY')
 list_names = get_lists(board_id, trello_token, trello_key)
+
 
 @app.route('/')
 def index():
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,6 +1,8 @@
-from flask import requests
+import requests
 
-_DEFAULT_ITEMS=[]
+_DEFAULT_ITEMS = []
+
+
 # board_id = os.getenv(TRELLO_BOARD_ID)
 # trello_token = os.getenv(TRELLO_TOKEN)
 # trello_key = os.getenv(TRELLO_KEY)
@@ -14,9 +16,11 @@ def get_items(board_id, trello_token, trello_key, list_names):
         list: The list of saved items.
     """
 
-    trello_get_cards_api = requests.get(f"https://api.trello.com/1/boards/{board_id}/cards/open?key={trello_key}&token={trello_token}").json
-    _DEFAULT_ITEMS = _select_fields(trello_get_cards_api)
-    return trello_get_cards_api
+    trello_get_cards_api = requests.get(
+        f"https://api.trello.com/1/boards/{board_id}/cards/open?key={trello_key}&token={trello_token}").json()
+    items = _select_fields(trello_get_cards_api, list_names)
+    return items
+
 
 def _select_fields(data, list_names):
     for record in data:
@@ -26,43 +30,48 @@ def _select_fields(data, list_names):
             title = record['name']
             list_id = record["idList"]
             status = list_names[list_id]
-            new_item = {"id": id, "status": status ,"title": title }
+            new_item = {"id": id, "status": status, "title": title}
             _DEFAULT_ITEMS.append(new_item)
-    return _DEFAULT_ITEMS
+    return _DEFAULT_ITEMS.copy()
 
 
-def get_lists(board_id,trello_token, trello_key):
-    trello_lists_api = requests.get(f"https://api.trello.com/1/boards/{board_id}/lists?key={trello_key}&token={trello_token}").json
+def get_lists(board_id, trello_token, trello_key):
+    trello_lists_api = requests.get(
+        f"https://api.trello.com/1/boards/{board_id}/lists?key={trello_key}&token={trello_token}").json()
     list_names = {}
-    for list in trello_lists_api:
-        list_names[list['id']] = list['name']
+    for list_name in trello_lists_api:
+        list_names[list_name['id']] = list_name['name']
     return list_names
-        
-def get_item(id):
-    """
-    Adds a new item with the specified title to the session.
 
-    Args:
-        title: The title of the item.
 
-    Returns:
-        item: The saved item.
-    """
-
-def add_item(title):
-    """
-    Adds a new item with the specified title to the session.
-
-    Args:
-        title: The title of the item.
-
-    Returns:
-        item: The saved item.
-    """
-def save_item(item):
-    """
-    Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
-
-    Args:
-        item: The item to save.
-    """
+# def get_item(id):
+#     """
+#     Adds a new item with the specified title to the session.
+#
+#     Args:
+#         title: The title of the item.
+#
+#     Returns:
+#         item: The saved item.
+#     """
+#
+#
+# def add_item(title):
+#     """
+#     Adds a new item with the specified title to the session.
+#
+#     Args:
+#         title: The title of the item.
+#
+#     Returns:
+#         item: The saved item.
+#     """
+#
+#
+# def save_item(item):
+#     """
+#     Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
+#
+#     Args:
+#         item: The item to save.
+#     """
