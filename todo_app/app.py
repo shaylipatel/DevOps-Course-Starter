8cb92b735f294951c55cde5948ac309c7fe3a9b9
shaylipatel <80635019+shaylipatel@users.noreply.github.com>
2023-09-29 14:36:21 +0100

    Moved functions into Trello class

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from todo_app.flask_config import Config
-from todo_app.data.trello_items import get_items, add_item, get_list_id, change_status
+from todo_app.data.trello_items import Trello
 
 app = Flask(__name__)
 app.config.from_object(Config())
-
+trello = Trello()
 
 @app.route('/')
 def index():
-    items = get_items()
+    items = trello.get_items()
     return render_template('index.html', items=items)
 
 
@@ -16,27 +16,23 @@ def index():
 def new_item():
     title = request.form.get('item_title')
     status = request.form.get('status')
-    list_id = get_list_id(status)
-    add_item(list_id, title)
+    trello.add_item(status, title)
     return redirect(url_for('index'))
 
 
 @app.route('/<item_id>/done', methods=['POST'])
 def move_to_done(item_id):
-    list_id = get_list_id('Done')
-    change_status(item_id, list_id)
+    trello.change_status(item_id, status='Done')
     return redirect(url_for('index'))
 
 
 @app.route('/<item_id>/in_progress', methods=['POST'])
 def move_to_in_progress(item_id):
-    list_id = get_list_id('In Progress')
-    change_status(item_id, list_id)
+    trello.change_status(item_id, status='In Progress')
     return redirect(url_for('index'))
 
 
 @app.route('/<item_id>/to_do', methods=['POST'])
 def move_to_to_do(item_id):
-    list_id = get_list_id('To Do')
-    change_status(item_id, list_id)
+    trello.change_status(item_id, status='To Do')
     return redirect(url_for('index'))
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -2,10 +2,6 @@ import requests
 import os
 
 
-# board_id = os.getenv(TRELLO_BOARD_ID)
-# trello_token = os.getenv(TRELLO_TOKEN)
-# trello_key = os.getenv(TRELLO_KEY)
-
 class Trello:
     def __init__(self):
         self.board_id = os.getenv('TRELLO_BOARD_ID')
@@ -25,90 +21,82 @@ class Trello:
             f"https://api.trello.com/1/boards/{self.board_id}/lists?key={self.trello_key}&token={self.trello_token}").json()
         list_names = {}
         for list_name in trello_lists_api:
-            list_names[list_name['id']] = list_name['name']
+            list_names[list_name['name']] = list_name['id']
         return list_names
 
+    def get_items(self):
+        """
+        Fetches all items on Trello board via API
 
-class Item(Trello):
+        Returns:
+            list: The list of Trello items.
+        """
+        trello_get_cards_api = requests.get(
+            f"https://api.trello.com/1/boards/{self.board_id}/cards/open?key={self.trello_key}&token={self.trello_token}").json()
+        items = self._select_fields(trello_get_cards_api)
+        return items
+
+    def change_status(self, item_id, status):
+        """
+        Makes an api request to Trello to change the status of the selected item
+
+        Args:
+            item_id: The id of item that is being changed
+            status: The new status
+        """
+        list_id = self._get_list_id(status)
+        requests.request("PUT",
+                         f"https://api.trello.com/1/cards/{item_id}?key={self.trello_key}&token={self.trello_token}&idList={list_id}")
+
+    def add_item(self, status, title):
+        """
+        Adds a new item with the specified title and status.
+
+        Args:
+            status: The status for the new item.
+            title: The title of the item.
+
+        """
+        list_id = self._get_list_id(status)
+
+        requests.post(
+            f"https://api.trello.com/1/cards?idList={list_id}&key={self.trello_key}&token={self.trello_token}&name={title}")
+
+    def _get_list_id(self, status):
+        """
+        Retrieves the list id for a list with a given status
+        Args:
+            status: The status of the item in a string
+
+        Returns:
+            str: the list id for the given status
+
+        """
+        return self.lists.get(status)
+
+    def _select_fields(self, data):
+        """
+        Iterates through the Trello api data and puts the item's name. status and id into a new class object
+        Args:
+            data: Trello api data in json format.
+
+        Returns:
+            list: List of Trello items.
+
+        """
+        items = []
+        for record in data:
+            list_id = record["idList"]
+            for key, value in self.lists.items():
+                if value == list_id:
+                    status = key
+            item = Item(record['id'], record['name'], status)
+            items.append(item)
+        return items
+
+
+class Item:
     def __init__(self, item_id, name, status):
-        super().__init__()
         self.id = item_id
         self.name = name
         self.status = status
-
-
-def get_items():
-    """
-    Fetches all items on Trello board via API
-
-    Returns:
-        list: The list of Trello items.
-    """
-    trello_details = Trello()
-    trello_get_cards_api = requests.get(
-        f"https://api.trello.com/1/boards/{trello_details.board_id}/cards/open?key={trello_details.trello_key}&token={trello_details.trello_token}").json()
-    items = _select_fields(trello_get_cards_api, trello_details.lists)
-    return items
-
-
-def _select_fields(data, list_names):
-    """
-    Iterates through the Trello api data and puts the item's name. status and id into a new class object
-    Args:
-        data: Trello api data in json format.
-        list_names: Dictionary of the list names and list ids.
-
-    Returns:
-        list: List of Trello items.
-
-    """
-    items = []
-    for record in data:
-        list_id = record["idList"]
-        status = list_names[list_id]
-        item = Item(record['id'], record['name'], status)
-        items.append(item)
-    return items
-
-
-def change_status(item_id, list_id):
-    """
-    Makes an api request to Trello to change the status of the selected item
-
-    Args:
-        item_id: The id of item that is being changed
-        list_id: The id of the list that the item should be moved to
-    """
-    trello_details = Trello()
-    requests.request("PUT",
-                     f"https://api.trello.com/1/cards/{item_id}?key={trello_details.trello_key}&token={trello_details.trello_token}&idList={list_id}")
-
-
-def add_item(list_id, title):
-    """
-    Adds a new item with the specified title and status.
-
-    Args:
-        list_id: The id of the list that the item is in.
-        title: The title of the item.
-
-    """
-    trello_details = Trello()
-    requests.post(
-        f"https://api.trello.com/1/cards?idList={list_id}&key={trello_details.trello_key}&token={trello_details.trello_token}&name={title}")
-
-
-def get_list_id(status):
-    """
-    Retrieves the list id for a list with a given status
-    Args:
-        status: The status of the item in a string
-
-    Returns:
-        str: the list id for the given status
-
-    """
-    trello_details = Trello()
-    for key, value in trello_details.lists.items():
-        if value == status:
-            return key
