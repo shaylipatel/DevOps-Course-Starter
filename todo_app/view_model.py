78155f941392439ea921cb88c174f8a477abbb88
shaylipatel <80635019+shaylipatel@users.noreply.github.com>
2023-10-06 15:32:30 +0100

    Added view_model.py module to view certain items

--- todo_app/view_model.py
+++ todo_app/view_model.py
@@ -5,3 +5,27 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+
+    @property
+    def done_items(self):
+        done_list = []
+        for item in self.items:
+            if item.status == 'Done':
+                done_list.append(item)
+        return done_list
+
+    @property
+    def to_do_items(self):
+        to_do_list = []
+        for item in self.items:
+            if item.status == 'To Do':
+                to_do_list.append(item)
+        return to_do_list
+
+    @property
+    def in_progress_items(self):
+        in_progress_list = []
+        for item in self.items:
+            if item.status == 'In Progress':
+                in_progress_list.append(item)
+        return in_progress_list
