90cf23a06ca8ed47dea6b26a9ea82774a1b66e7a
shaylipatel <80635019+shaylipatel@users.noreply.github.com>
2023-11-07 22:04:35 +0000

    Refactored to use list comprehension

--- todo_app/view_model.py
+++ todo_app/view_model.py
@@ -8,24 +8,12 @@ class ViewModel:
 
     @property
     def done_items(self):
-        done_list = []
-        for item in self.items:
-            if item.status == 'Done':
-                done_list.append(item)
-        return done_list
+        return [item for item in self.items if item.status == 'Done']
 
     @property
     def to_do_items(self):
-        to_do_list = []
-        for item in self.items:
-            if item.status == 'To Do':
-                to_do_list.append(item)
-        return to_do_list
+        return [item for item in self.items if item.status == 'To Do']
 
     @property
     def in_progress_items(self):
-        in_progress_list = []
-        for item in self.items:
-            if item.status == 'In Progress':
-                in_progress_list.append(item)
-        return in_progress_list
+        return [item for item in self.items if item.status == 'In Progress']
